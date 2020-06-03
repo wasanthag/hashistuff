0e86340bff5614b55ca5dc4313e11bbc63598651
Wasantha Gamage <wasanthag1302@gmail.com>
2020-06-03 15:00:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,25 @@ provider "aws" {
   region  = var.region
   version = "~> 2.54.0"
 }
+data "template_file" "userdata_win" {
+  template = <<EOF
+<script>
+echo "" > _INIT_STARTED_
+net user ${var.INSTANCE_USERNAME} /add /y
+net user ${var.INSTANCE_USERNAME} ${var.INSTANCE_PASSWORD}
+net localgroup administrators ${var.INSTANCE_USERNAME} /add
+echo ${base64encode(file("./test.txt"))} > tmp2.b64 && certutil -decode tmp2.b64 C:/test.txt
+echo "" > _INIT_COMPLETE_
+</script>
+<persist>false</persist>
+EOF
+}
 
 resource "aws_instance" "exam" {
   ami           = var.ami_id
   instance_type = var.inst_type
   key_name      = var.key_pair
+  user_data = data.template_file.userdata_win.rendered
 
   tags = {
     Name = "exam"
