6e000a10d4a169cea530fbbfa8202a0c3b71000c
Wasantha Gamage <wasanthag1302@gmail.com>
2020-06-03 14:52:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 variable "ami_id" {
   description = "ami image id"
-  default     = "ami-0fc916cb89db443ce"
+  default     = "ami-01b670d1a5b2c1da7"
 }
 
 variable "inst_type" {
