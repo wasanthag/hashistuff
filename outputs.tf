754130ea722cae47b62c1e1832b646a541316519
Wasantha Gamage <wasanthag1302@gmail.com>
2020-06-03 21:47:54 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,6 @@
 output "publicip" {
   value = "${aws_instance.tfec2.public_ip}"
 }
-output "Administrator_Password" {
- value = "${rsadecrypt(aws_instance.tfec2.password_data, file("${module.ssh_key_pair.private_key_filename}"))}"
-}
 output "all" {
   value = "${aws_instance.tfec2}"
 }
