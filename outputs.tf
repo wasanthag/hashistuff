271e0cbed67eb25f0c25e9087394a3b3eb9d2b35
Wasantha Gamage <wasanthag1302@gmail.com>
2020-06-03 21:14:29 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "publicip" {
   value = "${aws_instance.exam.public_ip}"
 }
 output "Administrator_Password" {
- value = "${rsadecrypt(aws_instance.this.password_data, file("${module.ssh_key_pair.private_key_filename}"))}"
+ value = "${rsadecrypt(aws_instance.tfec2.password_data, file("${module.ssh_key_pair.private_key_filename}"))}"
 }
 output "all" {
   value = "${aws_instance.exam}"
